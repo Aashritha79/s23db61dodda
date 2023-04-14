c7b06798746c571a0550e43ee681166240a8ca4b
Aashritha79 <aashritha.dodda79@gmail.com>
2023-04-14 10:42:29 -0500

    changes done

--- controllers/airlines.js
+++ controllers/airlines.js
@@ -21,6 +21,30 @@ exports.airlines_list = async function(req, res) {
     }
    };
 // for a specific Airlines.
+// for a specific Costume.
+exports.airlines_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Airlines.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
+// for a specific Costume.
+exports.costume_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await Costume.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
+
 exports.airlines_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: Airlines detail: ' + req.params.id);
 };
